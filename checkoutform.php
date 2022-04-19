b845a6247b9ebbf19bee05821cafbe94e1f106d6
prankin40 <pnr59272@uga.edu>
2022-04-18 20:08:04 -0400

    + - additions in cart

--- checkoutform.php
+++ checkoutform.php
@@ -6,10 +6,13 @@
 
   $user_data = check_login($con);
 
-  $queryProducts = 'SELECT * FROM products, cart WHERE products.productID = cart.productID ORDER BY products.productID';
+  $queryProducts = 'SELECT DISTINCT * FROM products, cart WHERE products.productID = cart.productID ORDER BY products.productID';
   $products = mysqli_query($con, $queryProducts);
   $result_count = mysqli_num_rows($products);
+  $queryAllProducts = 'SELECT * FROM products, cart WHERE products.productID = cart.productID ORDER BY products.productID';
   $sum = 0;
+  $products1 = mysqli_query($con, $queryAllProducts);
+  $result_count1 = mysqli_num_rows($products1);
 
   
 ?>
@@ -180,9 +183,11 @@
   </div>
   <div class="col-25">
     <div class="container">
-      <h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> <b><?php echo $result_count; ?></b></span></h4>
+      <h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> <b><?php echo $result_count1; ?></b></span></h4>
       <?php foreach ($products as $product) : ?>
-        <p><a href="marketplace.php"><?php echo $product['productName']; ?></a> <span class="price">$<?php echo $product['listPrice']; ?></span></p>
+        <p><a href="marketplace.php"><?php echo $product['productName']; ?></a> <a href="addToCart2.php?productID=<?php echo $product['productID']; ?>"> + </a> <a href="removeFromCart2.php?productID=<?php echo $product['productID']; ?>"> - </a> <span class="price">$<?php echo $product['listPrice']; ?></span></p>
+        <?php endforeach; ?>
+      <?php foreach ($products1 as $product) : ?>        
         <?php $sum+=$product['listPrice']; ?>
       <?php endforeach; ?>
       <hr>
