45ce5bbfc4e84d137c19461533e16116c56034b8
Larraine Mensah <clmensah@uga.edu>
2022-04-19 10:24:57 -0400

    fully functional inventory manager

---
+++ updateQuery.php
@@ -0,0 +1,50 @@
+<?php
+	session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+	$user_data = check_login($con);
+	$type = check_if_admin($con);
+	$id = $_GET['productID'];
+	$queryTitle = "SELECT productName FROM products WHERE categoryID = 2 AND productID = '$id'";
+	$result = mysqli_query($con, $queryTitle);
+	$title = mysqli_fetch_row($result)[0];
+?>
+<!doctype HTML>
+<html lang="english">
+
+<head>
+	<title>Successfully updated inventory</title>
+	<link rel="stylesheet" href="css/account.css">
+	<center>
+		<h1>Successfully updated <?php echo $title ?></h1>
+		<?php 	
+		foreach($_GET as $key => $value) {
+			if ($key == "productID") {
+				continue;
+			} else if(isset($_GET[$key])) {
+				if ($value == 0) {
+					$queryElement = "UPDATE products SET $key=$value WHERE categoryID = 2 AND productID = $id";
+					$update = mysqli_query($con, $queryElement);	
+				
+				} else if (!empty($_GET[$key])){
+					$queryElement3 = "UPDATE products SET $key='$value' WHERE categoryID = 2 AND productID = $id";
+					$update3 = mysqli_query($con, $queryElement3);	
+
+				}
+			}
+		}	
+		?>
+		<p> <a href="manageInventory.php">Click here to go back to manage inventory page.</a></p>
+	</center>
+</head>
+
+<body>
+	<center>
+
+	</center>
+</body>
+
+
+</html>
---
+++ vendorInterface.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+?>
+
+<!doctype HTML>
+<html lang="english">
+
+<head>
+	<link rel="stylesheet" href="css/account.css">
+	<center>
+		<h1>Vendor Controls</h1>
+	</center>
+</head>
+
+<body>
+	<center>
+		<p>Welcome vendor!</p><br>
+		<div class='container' style='display:flex'>
+			<div id="box">
+				<center><b>Reports</b></center>
+				<center><a href=salesReport.php>sales report</a></center>
+				<center><a href=orderReport.php>order report</a></center>
+				<center><a href=invReport.php>inventory report</a></center>
+			</div>
+			<div id="box">
+				<center><a href="manageInventory.php"> <b>View/Manage Inventory</b></a></center>
+			</div>
+			<div id="box">
+				<center><a href="manageOrders.php"> <b>View/Manage Orders</b></a></center>
+			</div>
+		</div>
+	</center>
+
+</body>
+
+</html>
