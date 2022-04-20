80f7f86e6027c9fc20dcadb9891320fb2a023980
Larraine Mensah <clmensah@uga.edu>
2022-04-20 09:13:37 -0400

    removed alter productID attempt + comments

--- addQuery.php
+++ addQuery.php
@@ -5,23 +5,16 @@
 	include("functions.php");
 
 	$user_data = check_login($con);
-    $type = check_if_vendor_and_admin($con);
+	$type = check_if_vendor_and_admin($con);
 
 	$noBlanks = true;
 		foreach($_GET as $key => $value) {
-			// if(isset($_GET[$key])) {
-				// if ($value == 0) {
-				// 	$queryElement = "UPDATE products SET $key=$value WHERE categoryID = 2 AND productID = $id";
-				// 	$update = mysqli_query($con, $queryElement);	
-				// }
 			if ($value != 0 && empty($value)){
-				// echo $value;
 				$noBlanks = false;
 				break;
 			}
 		}	
 		if ($noBlanks == true) {	
-			// echo "no blanks found";
 			$productCode = $_GET['productCode'];
 			$productName = $_GET['productName'];
 			$author = $_GET['author'];
@@ -31,14 +24,8 @@
 			$genre = $_GET['genre'];
 			$isBestSeller = $_GET['isBestSeller'];
 			$imagePath = $_GET['imagePath'];
-			$prevIDQuery = "SELECT MAX('productID') FROM products";
-			$result = intval(mysqli_query($con, $prevIDQuery));
-			$result++;
-			$resetIDQuery = "ALTER TABLE 'products' AUTO_INCREMENT = $result";
-			$result2 = mysqli_query($con, $resetIDQuery);
-			// $newID = intval($result);
 			$insertQuery = "INSERT INTO products(`categoryID`, `productCode`, `productName`, `author`, `ISBN`, `listPrice`, `stock`, `genre`, `isBestSeller`, `imagePath`, `inCart`) VALUES (2, '$productCode', '$productName', '$author', $isbn, $price, $stock, '$genre', $isBestSeller, '$imagePath', 0)";
-			$result3 = mysqli_query($con, $insertQuery);
+			$result = mysqli_query($con, $insertQuery);
 		} else {
 			echo '<script>alert("No blank input allowed")</script>';
 			echo "<script type='text/javascript'>document.location.href='addItem.php';</script>";
