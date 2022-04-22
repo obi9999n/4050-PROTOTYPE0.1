48a4bdec71b2955ef33ea7e84f78f1ed819b7565
prankin40 <pnr59272@uga.edu>
2022-04-22 15:07:56 -0400

    if in store pick up, hide cc info section

--- checkoutform.php
+++ checkoutform.php
@@ -124,65 +124,77 @@
     <div class="container">
       <form action="/action_page.php">
       
-        <div class="row">
-          <div class="col-50">
-            <h3>Shipping Address</h3>
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="Steve Jobs">
-            <label for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" placeholder="john@example.com">
-            <label for="reserve"> <input type="checkbox" name="reserve"> Pick-up Order in Store </label>
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input type="text" id="adr" name="address" placeholder="45 Baxter Street">
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <input type="text" id="city" name="city" placeholder="Athens">
+        <label for="reserve"> <input type="button" name="reserve" onClick="myFunction()" value="Pick-up Order in Store (Pay with Cash)" class="btn">  </label>
+        <div id="myDIV">
+          <div class="row">
+            
+            <div class="col-50">
+              <h3>Shipping Address</h3>
+              <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+              <input type="text" id="fname" name="firstname" placeholder="Steve Jobs">
+              <label for="email"><i class="fa fa-envelope"></i> Email</label>
+              <input type="text" id="email" name="email" placeholder="john@example.com">
+              
+              <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+              <input type="text" id="adr" name="address" placeholder="45 Baxter Street">
+              <label for="city"><i class="fa fa-institution"></i> City</label>
+              <input type="text" id="city" name="city" placeholder="Athens">
 
-            <div class="row">
-              <div class="col-50">
-                <label for="state">State</label>
-                <input type="text" id="state" name="state" placeholder="GA">
+              <div class="row">
+                <div class="col-50">
+                  <label for="state">State</label>
+                  <input type="text" id="state" name="state" placeholder="GA">
+                </div>
+                <div class="col-50">
+                  <label for="zip">Zip</label>
+                  <input type="text" id="zip" name="zip" placeholder="30606">
+                </div>
               </div>
-              <div class="col-50">
-                <label for="zip">Zip</label>
-                <input type="text" id="zip" name="zip" placeholder="30606">
+            </div>
+
+            <div class="col-50">
+              <h3>Payment</h3>
+              <label for="fname">Accepted Cards</label>
+              <div class="icon-container">
+                <i class="fa fa-cc-visa" style="color:navy;"></i>
+                <i class="fa fa-cc-amex" style="color:blue;"></i>
+                <i class="fa fa-cc-mastercard" style="color:red;"></i>
+                <i class="fa fa-cc-discover" style="color:orange;"></i>
+              </div>
+              <label for="cname">Name on Card</label>
+              <input type="text" id="cname" name="cardname" placeholder="Steve Jobs">
+              <label for="ccnum">Credit card number</label>
+              <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
+              <label for="expmonth">Exp Month</label>
+              <input type="text" id="expmonth" name="expmonth" placeholder="January">
+              <div class="row">
+                <div class="col-50">
+                  <label for="expyear">Exp Year</label>
+                  <input type="text" id="expyear" name="expyear" placeholder="2022">
+                </div>
+                <div class="col-50">
+                  <label for="cvv">CVV</label>
+                  <input type="text" id="cvv" name="cvv" placeholder="123">
+                </div>
               </div>
             </div>
           </div>
-
-          <div class="col-50">
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-              <i class="fa fa-cc-visa" style="color:navy;"></i>
-              <i class="fa fa-cc-amex" style="color:blue;"></i>
-              <i class="fa fa-cc-mastercard" style="color:red;"></i>
-              <i class="fa fa-cc-discover" style="color:orange;"></i>
-            </div>
-            <label for="reserve"> <input type="checkbox" name="reserve"> Cash (Pick-up Order in Store) </label>
-            <label for="cname">Name on Card</label>
-            <input type="text" id="cname" name="cardname" placeholder="Steve Jobs">
-            <label for="ccnum">Credit card number</label>
-            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
-            <label for="expmonth">Exp Month</label>
-            <input type="text" id="expmonth" name="expmonth" placeholder="January">
-            <div class="row">
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2022">
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label>
-                <input type="text" id="cvv" name="cvv" placeholder="123">
-              </div>
-            </div>
-          </div>
-          
+          <label>
+            <input type="checkbox" checked="checked" name="sameadr"> Billing Address same as Shipping Address
+          </label>
         </div>
-        <label>
-          
-          <input type="checkbox" checked="checked" name="sameadr"> Billing Address same as Shipping Address
-        </label>
         <input type="submit" value="Continue to checkout" class="btn">
+        <script>
+          function myFunction() {
+            var x = document.getElementById("myDIV");
+            if(x.style.display === "none") {
+              x.style.display = "block";
+            } else {
+              x.style.display = "none";
+            }
+          }
+        </script>
+
       </form>
     </div>
   </div>
@@ -202,12 +214,10 @@
         <input type="submit" value="Add Promo" class="btn">
         
         <?php foreach ($promos as $promo) : ?>
-          <?php if ($_REQUEST['code'] == $promo['promocode']) : ?>
+          <?php if (isset($_REQUEST['code']) == $promo['promocode']) : ?>
             <p>Total with Promo <span class="price" style="color:black"><b>$<?php echo $sum * ((100 - $promo['promopercent'])/100);?></b></span></p>
           <?php endif;?> 
         <?php endforeach; ?>
-          
-        
       </form>
     </div>
   </div>
