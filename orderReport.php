248813752aedcdf9f2fe5960b76849c5fb7430ee
dariusnwadike <dariusnwadike@gmail.com>
2022-04-24 19:28:46 -0400

    fixed orderReport.php

--- orderReport.php
+++ orderReport.php
@@ -8,13 +8,19 @@ session_start();
     // $type = check_if_vendor_and_admin($con);
 //   $orders = query on orders table and return number of rows
 //	$totalSales = query on orders table and return sum of all totals
-    $query1 =  "SELECT COUNT(*) FROM orders";
+    $query1 =  "SELECT * FROM orders";
     $result1 = mysqli_query($con, $query1);
+    $result_count1 = mysqli_num_rows($result1);
     // $result1b = mysqli_fetch_row($result)[0];
-    $query2 =  "SELECT COUNT(*) FROM 'orders' WHERE 'in_store' = 1";
+    $query2 =  "SELECT * FROM orders WHERE in_store = 1";
     $result2 = mysqli_query($con, $query2);
-    $query3 = " SELECT SUM(total) FROM 'orders'";
-    $result3 = mysqli_query($con, $query3);
+    $result_count2 = mysqli_num_rows($result2);
+    
+    $allSales = "SELECT * FROM orders";
+    $sales = mysqli_query($con, $allSales);
+    foreach ($sales as $sale) :
+        $sum += $sale['total'];
+    endforeach;
 
 ?>
 
@@ -32,9 +38,9 @@ session_start();
     <button onclick="location.href='vendor.php'" style="margin: 15px;">Go back</button>
     <div id="box">
         <center>
-            <p>Orders placed: <?php echo intval($result1);?></p>
-            <p>Pick-up orders: <?php echo $result2;?></p>
-            <p>Total sales: <?php echo $result3;?></p>
+            <p>Orders placed: <?php echo $result_count1;?></p>
+            <p>Pick-up orders: <?php echo $result_count2;?></p>
+            <p>Total sales: <?php echo $sum;?></p>
         </center>
     </div>
 </body>
